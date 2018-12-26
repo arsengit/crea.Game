e2b72592cd9d090ed567de6ba4bc7d50d43a9756
Arsenaljs <alkamyan97@gmail.com>
2018-12-27 00:34:04 +0400

    newCommit

--- script/script.js
+++ script/script.js
@@ -2,8 +2,11 @@ const rows = 10;
 const cols = 10;
 let colors = ["blue", "red", "green", "orange"];
 const container = document.getElementById("container");
+let checked = {};
+let matches = [];
 
 const ballsList = [];
+const nodeList = {};
 
 function build() {
     for (let i = 1; i <= rows; i++) {
@@ -16,20 +19,49 @@ function build() {
             const color = colors[Math.floor(Math.random() * colors.length)];
             ball.style.backgroundColor = color;
             ball.dataset.color = color;
-            container.appendChild(ball);
             ball.addEventListener("click", handleClick);
-            ballsList[i][j] = ball;
+            nodeList['key'+i+j] = ball;
+            container.appendChild(ball);
+            ballsList[i][j] = {
+                row: i,
+                column: j,
+                color: color
+            };
         }
     }
 }
 
-function checkColor () {
-    
+function checkColor(color, row, column) {
+    if(!('key'+row+column in checked)) {
+        checked['key'+row+column] = ballsList[row][column];
+        if(ballsList[row][column].color === color) {
+            // console.log(ballsList[row][column]);
+            matches.push(ballsList[row][column]);
+            if(row - 1 > 0) checkColor(color, row - 1, column);
+            if(row + 1 <= rows) checkColor(color, row + 1, column);
+            if(column - 1 > 0) checkColor(color, row, column - 1);
+            if(column + 1 <= cols) checkColor(color, row, column + 1);
+        }
+    }
 }
 
+function handleClick (eventObj) {
+    checked = {};
+    matches = [];
+    const { color, row, column } = eventObj.target.dataset;
+    checkColor(color, +row, +column);
+    if(matches.length > 2) {
+        matches.forEach((ball) => {
+            nodeList['key'+ball.row+ball.column].style.backgroundColor = 'white';
+            const color = colors[Math.floor(Math.random() * colors.length)];
+            nodeList['key'+ball.row+ball.column].dataset.color = color;
+            ballsList[ball.row][ball.column].color = color;
 
-function handleClick () {
-    
+            setTimeout(() => {
+                nodeList['key'+ball.row+ball.column].style.backgroundColor = color;
+            }, 500);
+        })
+    }
 }
 
 build();
